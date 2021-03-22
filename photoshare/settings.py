1f7a43f59817eddd41dfe64a9119fb66fc426c33
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-22 15:47:12 +0300

    use postgres database

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photo',
+        'USER': 'moringaaccess',
+        'PASSWORD': '1234',
     }
 }
 
@@ -119,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
