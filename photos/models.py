d7fd1ba0b48c176e237147c816defc751892139a
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-22 15:41:08 +0300

    define save functionality

--- photos/models.py
+++ photos/models.py
@@ -7,10 +7,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def save_travel(self):
+        self.save()
+    
+
 class Photo(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(blank = False)
     description = models.TextField(max_length=500, blank=False)
 
     def __str__(self):
         return self.description
+
+    def save_photo(self):
+        self.save()
+    
+   
