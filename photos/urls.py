7cbe65e09cc882e117352fcf0c73a8d7bcc3bca8
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-22 21:33:07 +0300

    add home route

--- photos/urls.py
+++ photos/urls.py
@@ -3,7 +3,9 @@ from .import views
 
 
 urlpatterns = [
-    path('',views.gallery, name = 'gallery'),
+    path('', views.welcome, name = 'welcome'),
+    path('gallery/',views.gallery, name = 'gallery'),
     path('photo/<str:pk>/',views.viewPhoto, name = 'photo'),
     path('add/',views.addPhoto, name = 'add'),
+    
 ]
