724ced57c53de91b3008e82c9e699ddbc15f7a82
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-22 15:47:43 +0300

    register category and photo to admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import Photo, Category
+from .models import Category, Photo
 
 admin.site.register(Category)
 admin.site.register(Photo)
