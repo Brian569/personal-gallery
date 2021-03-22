5f34af7a98b8a9bab9cd131188073bba562ba3d1
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-22 15:39:28 +0300

    run model test for category & photos

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Category, Photo
 
-# Create your tests here.
+
+class CategoryTestClass(TestCase):
+
+    def setUp(self):
+
+        self.travel = Category(name= 'Travel')
+
+    def test_instance(self):
+
+        self.assertTrue(isinstance(self.travel, Category))
+
+    def test_save_method(self):
+
+        self.travel.save_travel()
+        travels = Category.objects.all()
+        self.assertTrue(len(travels)>0)
+
+
+class PhotoTestClass(TestCase):
+
+    def setUp(self):
+        self.travel = Category(name= 'Travel')
+        self.image = Photo(category=self.travel, description = 'Am in the test module', )
+
+    def test_instance(self):
+
+        self.assertTrue(isinstance(self.image, Photo))
+
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Photo.objects.all().delete()
